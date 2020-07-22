1b924a83e89651cc79a871c7b61ac63b840787ee
afteralec <github email address>
2020-07-21 23:03:02 -0700

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,59 @@
 class Startup
+  attr_reader :founder, :domain
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name, @founder, @domain = name, founder, domain
+
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  #Instance methods that assign data
+  def pivot(string)
+    @domain = string
+  end
+
+  def sign_contract(venture_capitalist, type, investment)
+    FundingRound.new(self, venture_capitalist, type, investment)
+  end
+
+  #Instance methods that fetch data
+  def funding_rounds
+    FundingRound.all.select { |funding_round| funding_round.startup == self }
+  end
+
+  def num_funding_rounds
+    funding_rounds.length
+  end
+
+  def total_funds
+    funding_rounds.map(&:investment).sum
+  end
+
+  def investors
+    funding_rounds.map(&:venture_capitalist).uniq
+  end
+
+  def big_investors
+    investors.select { |investor| VentureCapitalist.tres_comma_club.include?(investor) }
+  end
+
+  #Class methods that aggregate data
+  def self.find_by_founder(founder)
+    all.find { |startup| startup.founder == founder }
+  end
+
+  def self.domains
+    all.map(&:domain).uniq
+  end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,46 @@
 class VentureCapitalist
+  attr_reader :name, :total_worth
 
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name, @total_worth = name, total_worth
+
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  #Instance methods that assign data
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  #Instance methods that fetch data
+  def funding_rounds
+    FundingRound.all.select { |funding_round| funding_round.venture_capitalist == self }
+  end
+
+  def portfolio
+    funding_rounds.map(&:startup).uniq
+  end
+
+  def biggest_investment
+    funding_rounds.max_by(&:investment)
+  end
+
+  def invested(domain)
+    funding_rounds.select { |funding_round| funding_round.startup.domain.downcase == domain.downcase }.map(&:investment).sum
+  end
+
+  #Class methods that aggregate data
+  def self.tres_comma_club
+    all.select { |venture_capitalist| venture_capitalist.total_worth >  1000000 }
+  end
 end
