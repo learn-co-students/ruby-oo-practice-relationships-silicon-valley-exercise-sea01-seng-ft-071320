a6ada92d59afa54fd48fd8f1b204c6331b27e6cc
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-07-23 06:55:51 -0700

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,55 @@
 class Startup
 
+    attr_accessor :name
+    attr_reader :founder, :domain
+
+    @@all = []
+
+    def initialize (name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pivot (new_domain, new_name)
+        @name = new_name
+        @domain = new_domain
+        self
+    end
+#     
+    def self.find_by_founder(founder)
+        self.all.find {|startup| startup.founder == founder}
+    end
+    def self.domains
+        self.all.collect {|startup| startup.domain}
+    end 
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+    def rounds 
+        FundingRound.all.select {|round| round.startup == self}
+    end
+    def num_funding_rounds
+        rounds.count
+    end
+
+    def total_funds
+        rounds.sum {|round| round.investment}
+    end
+
+    def investors
+        rounds.collect {|round| round.venture_capitalist}.uniq
+    end
+
+    def big_investors 
+        big = investors.select {|investor| investor.total_worth > 1000000000}
+        big.collect {|investor| investor.name}
+    end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+ ven1 = VentureCapitalist.new("Ven1", 1500000)
+ ven2 = VentureCapitalist.new("Ven2", 22500000)
+ ven3 = VentureCapitalist.new("Ven3", 1000000009)
+ ven4 = VentureCapitalist.new("Ven4", 402000)
+ ven5 = VentureCapitalist.new("Ven5", 99999993)
+ ven6 = VentureCapitalist.new("Ven6", 1000000009)
+
+startup1 = Startup.new("name1", "founder1", "domain1")
+startup2 = Startup.new("name2", "founder2", "domain2")
+startup3 = Startup.new("name3", "founder3", "domain3")
+startup4 = Startup.new("name4", "founder4", "domain4")
+
+found1 = FundingRound.new(startup1, ven1, "Angel", 150.5)
+found2 = FundingRound.new(startup1, ven2, "Angel", 175.3)
+found3 = FundingRound.new(startup3, ven3, "Pre-Seed", 135.5)
+found4 = FundingRound.new(startup2, ven6, "Seed", 129.8)
+found5 = FundingRound.new(startup1, ven1, "Series A", 134.0)
+found6 = FundingRound.new(startup4, ven1, "Series A", 150.5)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
